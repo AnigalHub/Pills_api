5a1f5e4e6289b2c247e75d7789984541f90b4809
AnigalHub <anigal1997@gmail.com>
2021-07-24 19:03:00 +0300

    добавлен запрос на удаление

--- api.srv/src/routes/index.js
+++ api.srv/src/routes/index.js
@@ -8,13 +8,19 @@ const db = require("./../db")
 
 //Получить список из базы
 api.get('/', asyncHandler(async (req, res) => {
-    const list = (await db.query('SELECT * FROM pills')).rows
+    const list = (await db.query('SELECT * FROM pills order by id')).rows
     res.json(list)
 }))
 //Добавить в базу
 api.post('/add', asyncHandler(async (req, res) => {
     const data = req.body
     await db.query('INSERT INTO pills (name, id, number, storage, year) values ($1, $2, $3, $4, $5)', [data.name, data.id, data.number, data.storage, data.year])
+    res.end()
+}))
+//Удалить элемент из бд
+api.post('/delete', asyncHandler(async (req, res) => {
+    const data = req.body
+    await db.query("DELETE FROM pills WHERE id = $1", [data.id])
     res.end()
 }))
 // Обработчик ошибок
