e5bccb3bd570cffb98a0a0e2fe974ffc402ab957
AnigalHub <anigal1997@gmail.com>
2021-07-24 12:10:52 +0300

    добавлен запрос на добавление в базу

--- api.srv/src/routes/index.js
+++ api.srv/src/routes/index.js
@@ -11,8 +11,12 @@ api.get('/', asyncHandler(async (req, res) => {
     const list = (await db.query('SELECT * FROM pills')).rows
     res.json(list)
 }))
-
-
+//Добавить в базу
+api.post('/add', asyncHandler(async (req, res) => {
+    const data = req.body
+    await db.query('INSERT INTO pills (name, id, number, storage, year) values ($1, $2, $3, $4, $5)', [data.name, data.id, data.number, data.storage, data.year])
+    res.end()
+}))
 // Обработчик ошибок
 api.use(errorCatcher)
 
