8123d4870f9358a3d9f653d23f6f2e4714b4c0b7
AnigalHub <anigal1997@gmail.com>
2021-08-16 17:18:53 +0300

    добавлено условие на существование у переменной значения, сокращено логически тело функций

--- api.srv/src/routes/index.js
+++ api.srv/src/routes/index.js
@@ -4,7 +4,7 @@ const api = require("express").Router()
 const { asyncHandler, errorCatcher } = require("../middlewares")
 // База
 const db = require("./../db")
-
+const buildQueryConstraints = require("../helpers/BuildQueryConstraints")
 
 //Получить список из базы
 api.get('/', asyncHandler(async (req, res) => {
@@ -27,86 +27,8 @@ api.post('/delete', asyncHandler(async (req, res) => {
 //Поиск по базе
 api.post('/search', asyncHandler(async (req, res) => {
     const  data = req.body
-/*
-   let request, parametr;
-   if(data.name != "" && data.storage !=""){
-       request = "name = ($1) and storage = ($2)";
-       parametr = [data.name, data.storage]
-       console.log(parametr)
-       console.log(request )
-   }
-
-  else if(data.name != ""){
-       request = "name = ($1)";
-       parametr = [data.name]
-       console.log(parametr)
-   }
-   else{
-       request = "storage = ($1)";
-       parametr = [data.storage]
-       console.log(parametr)
-   }
-*/
-
     let request_parameters ={name:data.name, storage:data.storage,year:data.year}
-    let arr = [];
-    let request;
-
-    //Создаем новый массив значений объекта (без пустых значений (''))
-    function FillArray(obj) {
-        arr = Object.values(obj).filter(n => n)
-        return arr
-    }
-    //Создаем условия запроса
-    function CreateRequestConditions(obj){
-        let arr_key =[]
-        let arr_request = []
-
-        //создаем массив ключей объекта (которые без пустых значений (''))
-        for (let key in obj) {
-            if (obj[key] != ""){
-                arr_key.push(key)
-            }
-        }
-        //проходимся по массиву ключей и создаем строку в зависимости от длины массива и заменяем , на and
-        for (let i=0; i<arr_key.length;i++){
-            if(arr[i] != "" && arr[i+1] != ""){
-                let a = `${arr_key[i]} = ($${i+1})`;
-                arr_request.push(a)
-                request = arr_request.join().split(',').join(' and ');
-            }
-        }
-        return request;
-    }
-    //console.log(FillArray(request_parameters))
-    //console.log(CreateRequestConditions(request_parameters))
-
-
-   /* let arr = Object.values(obj).filter(n => n)
-    console.log(arr)
-    let arr_key =[]
-    let arr_request = []
-
-    for (let key in obj) {
-       if (obj[key] != ""){
-           arr_key.push(key)
-       }
-    }
-    for (let i=0; i<arr_key.length;i++){
-        if(arr[i] != "" && arr[i+1] != ""){
-            let a = `${arr_key[i]} = ($${i+1})`;
-            arr_request.push(a)
-            request = arr_request.join().split(',').join(' and ');
-        }
-    }
-    console.log(request)
-    console.log("новый")
-    console.log(arr)
-    console.log("____________")
-*/
-
-
-    const resp = (await db.query(`SELECT * from pills where ${CreateRequestConditions(request_parameters)}`, FillArray(request_parameters)))
+    const resp = (await db.query(`SELECT * from pills ${buildQueryConstraints.CreateRequestConditions(request_parameters)}`, buildQueryConstraints.GetArray(request_parameters)))
     const list = resp.rows
     res.json(list)
 }))
