5eb1fa3a19ef7c87e1cceba65fece7add952ac61
AnigalHub <anigal1997@gmail.com>
2021-08-19 20:10:25 +0300

    добавлен запрос на обновление update

--- api.srv/src/routes/index.js
+++ api.srv/src/routes/index.js
@@ -18,7 +18,12 @@ api.post('/delete', asyncHandler(async (req, res) => {
     await db.query("DELETE FROM pills WHERE id = $1", [data.id])
     res.end()
 }))
-
+//Изменить элемент в бд
+api.post('/update', asyncHandler(async (req, res) => {
+    const data = req.body
+    await db.query("UPDATE pills SET name = $1, number = $2, storage = $3, year = $4 WHERE id = $5;", [data.name, data.number, data.storage, data.year, data.id])
+    res.end()
+}))
 //Поиск по базе и вывод списка (всего или того, что ищем)
 api.post('/', asyncHandler(async (req, res) => {
     const  data = req.body
